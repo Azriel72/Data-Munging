7328c78b411a6c000523d40a3b9ee5e04fda0699
Azriel72 <124940320+Azriel72@users.noreply.github.com>
2024-05-28 12:10:29 -0400

    Update source_file.py

--- source_file.py
+++ source_file.py
@@ -1,5 +1,5 @@
 '''
     This are the variables containing the path to the source files.
 '''
-SOURCE_WEATHER = "D:\\universidad\\TENDENCIAS\\TEO\\weather_data.txt"
-SOURCE_FOOTBALL = "D:\\universidad\\TENDENCIAS\\TEO\\football_data.txt"
+SOURCE_WEATHER = "D:\\file\\path\\weather_data.txt"
+SOURCE_FOOTBALL = "D:\\file\\path\\football_data.txt"
